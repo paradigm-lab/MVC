23cd419e0da35b3578083bc3ec3a4b1f747f07b5
collins <collinsboniface1000@gmail.com>
2021-10-21 15:46:55 +0300

    Adding the main function for the spring to start running

--- src/main/java/com/student/StudentManagementSystemApplication.java
+++ src/main/java/com/student/StudentManagementSystemApplication.java
@@ -1,4 +1,9 @@
 package com.student;
 
+import org.springframework.boot.SpringApplication;
+
 public class StudentManagementSystemApplication {
+    public static void main(String args) {
+        SpringApplication.run(StudentManagementSystemApplication.class, args);
+    }
 }
