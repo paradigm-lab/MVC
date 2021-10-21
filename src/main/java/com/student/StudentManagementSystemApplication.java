3e2cc85923798ba5c61126269556c6bc5a46694e
collins <collinsboniface1000@gmail.com>
2021-10-21 17:39:45 +0300

    Fixing the problem for the display of the students from the database to the HTML table

--- src/main/java/com/student/StudentManagementSystemApplication.java
+++ src/main/java/com/student/StudentManagementSystemApplication.java
@@ -1,12 +1,32 @@
 package com.student;
 
+import com.student.entity.Student;
+import com.student.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
+    }
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Student student = new Student("Collins", "Morgan", "Collinsboniface1000@gmail.com");
+        studentRepository.save(student);
+
+        Student student1 = new Student("Ian", "Johnson", "Ianjohnson@gmail.com");
+        studentRepository.save(student1);
+
+        Student student2 = new Student("Johnny", "Papa", "Johnny@lepapa.com");
+        studentRepository.save(student2);
+
     }
 }
