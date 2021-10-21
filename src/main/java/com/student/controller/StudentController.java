1e919a91e5921ae5c674760b7925adeee8f33cf0
collins <collinsboniface1000@gmail.com>
2021-10-21 15:25:51 +0300

    Adding the controller

--- src/main/java/com/student/controller/StudentController.java
+++ src/main/java/com/student/controller/StudentController.java
@@ -1,2 +1,25 @@
-package com.student.controller;public class StudentController {
+package com.student.controller;
+
+import com.student.service.StudentService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        super();
+        this.studentService = studentService;
+    }
+
+
+    //A handler Method to handle list of student and return mode and view
+    @GetMapping("/students")
+    public String listStudents(Model model) {
+        model.addAttribute("student", studentService.getAllStudents());
+        return "students";
+    }
 }
