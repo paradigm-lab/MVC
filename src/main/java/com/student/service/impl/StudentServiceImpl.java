3f03047fa5b53aa0daf2e75834c1cf2a58d6ab89
collins <collinsboniface1000@gmail.com>
2021-10-21 15:27:51 +0300

    Adding the class the implements the interface of the StudentService

--- src/main/java/com/student/service/impl/StudentServiceImpl.java
+++ src/main/java/com/student/service/impl/StudentServiceImpl.java
@@ -1,2 +1,24 @@
-package com.student.service.impl;public class StudentServiceImpl {
+package com.student.service.impl;
+
+import com.student.entity.Student;
+import com.student.repository.StudentRepository;
+import com.student.service.StudentService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
 }
