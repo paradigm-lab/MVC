c1d39907e8cbc6f0f5675a2ada833c9c903ed99b
collins <collinsboniface1000@gmail.com>
2021-12-05 14:58:11 +0200

    Adding the shell script

---
+++ test.sh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+function testAddStudent {
+  BODY=$(cat <<EOF
+{
+  "name": "Bilal",
+  "email": "example@example.com",
+  "dob": "2000-01-08"
+}
+EOF
+)
+  curl -X POST -H "Content-Type: application/json" -d "$BODY" http://localhost:8080/api/v1/student 
+}
+
+# | jq
+function testGetStudents {
+  curl -X GET http://localhost:8080/api/v1/student | jq
+}
+
+
+function testDeleteStudent {
+  curl -X DELETE http://localhost:8080/api/v1/student/1
+}
+
+function testPutStudent {
+  curl -X PUT http://localhost:8080/api/v1/student/5 -d name=Maria&email=maria@gmail.com
+}
+
+###################################################################
+
+#testPutStudent
+#testDeleteStudent
+#testAddStudent
+testGetStudents
